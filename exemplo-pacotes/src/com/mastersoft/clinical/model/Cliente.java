38d5ceca4f1adcb87028c4608a6ee103abba139d
giuliano-marcus-bianco <giuliano.mbianco@gmail.com>
2024-09-29 17:16:28 -0300

    Add project exemplo-pacotes

---
+++ exemplo-pacotes/src/com/mastersoft/clinical/model/Cliente.java
@@ -0,0 +1,7 @@
+package com.mastersoft.clinical.model;
+
+public class Cliente {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
